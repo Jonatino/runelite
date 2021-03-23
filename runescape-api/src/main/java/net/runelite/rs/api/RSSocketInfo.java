cb63dfd73dec2850b310900a94fafb23d501fa53
Jonatino <jonathanbeaudoin1996@gmail.com>
2021-03-23 15:28:25 -0400

    Everything is working now :)

--- runescape-api/src/main/java/net/runelite/rs/api/RSSocketInfo.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSSocketInfo.java
@@ -3,12 +3,4 @@ package net.runelite.rs.api;
 import net.runelite.api.MouseRecorder;
 import net.runelite.mapping.Import;
 
-public interface RSSocketInfo
-{
-	@Import("worldPort")
-	int getWorldPort();
-	
-	@Import("worldPort")
-	void setWorldPort(int worldPort);
-	
-}
+public interface RSSocketInfo {}
